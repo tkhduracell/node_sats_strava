9d7e0a1a12f254ef3da53d346df8eeb07f88d307
Filip Lindqvist <buggfille@gmail.com>
2023-09-15 15:40:28 +0200

    Add intro text

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
         <h1>Welcome to SATS exporter</h1>
+        <p>This tool can be used to upload SATS activities to your Strava account.</p>
+        <ol>
+            <li>Login to your SATS account</li>
+            <li>Select activity to upload</li>
+            <li>Login to your Strava account</li>
+            <li>Done</li>
+        </ol>
         <NuxtLink to="/login">
             <Button>Start</Button>
         </NuxtLink>
