369572033b4c07aaf05521f6086f1a02c5999708
Filip Lindqvist <buggfille@gmail.com>
2023-10-13 15:20:55 +0200

    Setup cookies

--- server/lib/cookie.ts
+++ server/lib/cookie.ts
@@ -2,14 +2,17 @@ import { H3Event } from 'h3';
 
 export type SessionKey = 'Sats-JWT' | 'Sats-UserId' | 'Strava-JWT'
 
+type Cookieopts = Parameters<typeof setCookie>[3]
+const opts: Cookieopts = { httpOnly: true, sameSite: 'strict', secure: true, maxAge: 3600*356 }
+
 export function addSessionValues(event: H3Event, values: Partial<Record<SessionKey, string>>): Partial<Record<SessionKey, string>> {
     const json = getCookie(event, '__session')
     if (json) {
         const obj = JSON.parse(json) as Record<SessionKey, string>
-        setCookie(event, '__session', JSON.stringify({ ...obj, ...values }))
+        setCookie(event, '__session', JSON.stringify({ ...obj, ...values }), opts)
         return { ...obj, ...values }
     } else {
-        setCookie(event, '__session', JSON.stringify(values))
+        setCookie(event, '__session', JSON.stringify(values), opts)
         return values
     }
 }
